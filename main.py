c0c9b00b04d6c4f970bd5fd8a3898c14d8d140ad
Sylvia Awuor <sylviaawuor20@gmail.com>
2021-10-28 16:47:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+def find_jobs():
+    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Data+Analytics&txtLocation=&cboWorkExp1=0').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+    for index, job in enumerate(jobs):
+        published_date = job.find('span', class_='sim-posted').span.text
+        if 'few' in published_date:
+            company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
+            skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '')
+            more_info = job.header.h2.a['href']
+
+            # print(published_date)
+
+            # print(f'''
+            # Company Name: {company_name}
+            # Required Skills: {skills}
+            # ''')
+            with open(f'posts/{index}.txt', 'w') as f:
+                f.write(f"Company Name: {company_name.strip()} \n")
+                f.write(f"Required Skills: {skills.strip()} \n")
+                f.write(f"More Info: {more_info}")
+            print(f'File Saved: {index}')
+
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        time_wait = 10
+        print(f"Waiting {time_wait}minutes...")
+        time.sleep(time_wait * 60)
